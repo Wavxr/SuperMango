ad8482746ed1037c5bef67c6c1b281620f68494b
Wavxr <johnveraguilar25@gmail.com>
2025-05-18 12:02:51 +0800

    feat: add CORS middleware and update API endpoint to use HTTPS from NGROK

--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import core
 
 app = FastAPI(title="SuperMango API")
 
 app.include_router(core.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://gopher-loved-largely.ngrok-free.app",  
+        
+    ],
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 @app.get("/")
 def root():
